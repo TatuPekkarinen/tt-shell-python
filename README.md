04dfe50e34dcad3e0d354e40ea0307c84f5649e7
Tatu Pekkarinen <Tatupek7@gmail.com>
2025-11-16 01:15:49 +0200

    Colored terminal text, more readable along with other things I wanted to add

--- README.md
+++ README.md
@@ -2,21 +2,21 @@ Minimal Python Shell
 
 This project is a small command-line shell written in Python. It supports a few basic builtin commands and can also run executable files from the current directory. The goal is to provide a lightweight example of how a shell parses input and decides what to do with it.
 
-This was a codecrafters challenge I did without AI and im planning to add my own functionalities to the shell.
+Initially a codecrafters challenge I did without AI that includes a couple of differences to the initial challenge. Mostly made for me to tinker with like adding colorization making the file run commands as I actually need them to be alongside other commands that will be added.
 
 LIST OF USABLE COMMANDS
 
-            Exit - exits the program
-            Type - tells if the file is in the directory or if the command is builtin
-            Echo - echoes the text
-            ./ - executes a given file (for example ./cmd)
+    Exit - exits the program
+    Type - tells if the file is in the directory or if the command is builtin
+    Echo - echoes the text
+    Prefix "." - executes a given file (for example .cmd)
             
 CODECRAFTERS STARTER CODE
             
-            import sys
+    import sys
             
-            def main():
-                sys.stdout.write("$ ")
+    def main():
+        sys.stdout.write("$ ")
             
-            if __name__ == "__main__":
-                main()
+    if __name__ == "__main__":
+        main()
--- main.py
+++ main.py
@@ -1,24 +1,54 @@
+import time
 import sys
 import shutil
 import subprocess
+import datetime
+import os
+
+GREEN = '\033[92m'
+TITLE1 = '\033[94m'
+TITLE2 = '\033[95m'
+WARNING = '\033[91m'
+RESET = '\033[0m'
 
 commands = {"exit", "echo", "type"}
 
 #executing file
-def exec_file(execFind):
-    subprocess.Popen(execFind)
+def exec_file(execSpl):
+    def not_found(execSpl):
+        print(f"{WARNING}{execSpl[1]}file not found in the PATH.{RESET}")
+        return
+    
+    execPath = shutil.which(execSpl[1])
+
+    if os.access(str(execPath), os.X_OK) == True:
+            print(f"{GREEN}Opening the file /{RESET}", execPath)
+            time.sleep(1)
+            subprocess.run(execPath)
+
+    elif os.access(str(execPath), os.X_OK) == False:
+        not_found(execSpl)
+    else:
+        not_found(execSpl)
 
 #error message
 def error(cmd, cmdSpl):
-    match cmdSpl[0]:
-        case "type":
-            print(*cmdSpl[1:],": not found",sep="")
-        case _:
-            print(f"{cmd}: command not found")
+    print(f"{WARNING}", end="")
+    if cmd == "type":
+        print(f"{cmd}: syntax incorrect")
+    elif cmdSpl[1:] is not None:
+        match cmdSpl[0]:
+            case "type":
+                print(*cmdSpl[1:],": not found",sep="")
+            case _:
+                print(f"{cmd}: command not found")
+    print(f"{RESET}", end="")
+    return
 
 #echo command
 def echo_cmd(cmdSpl):
     print(* cmdSpl[1:])
+    return
 
 #exit command
 def exit_cmd(cmdSpl):
@@ -27,14 +57,16 @@ def exit_cmd(cmdSpl):
             sys.exit(0)
         case _:
             error(cmdSpl) 
+    return
 
 #type command
 def type_cmd(cmdSpl, cmd):
     if cmd == "type":
         error(cmdSpl, cmd)
         return
-    
+
     file_find = shutil.which(cmdSpl[1])
+
     if cmdSpl[1] in commands:
         print(cmdSpl[1], "is a shell builtin")
         return  
@@ -42,15 +74,20 @@ def type_cmd(cmdSpl, cmd):
         error(cmd, cmdSpl)            
     else: print(cmdSpl[1],"is", file_find)
 
+    return
+
 #executing commands
 def cmdexec():
-    sys.stdout.write("$ ")
+    sys.stdout.write(f"{GREEN}$ {RESET}")
     cmd = input()
+    file_prefix = cmd.find(".")
     cmdSpl = cmd.split(" ")
     execSpl = cmd.split(".")
     cmdSpl = cmd.split(" ")
 
     match cmdSpl[0]:
+        case "":
+            return
         case "echo":
             echo_cmd(cmdSpl)
         case "exit":
@@ -58,14 +95,16 @@ def cmdexec():
         case "type":
             type_cmd(cmdSpl, cmd)
         case _:
-            if  cmd == "." + execSpl[1]:
-                execFind = shutil.which(execSpl[1])
-                exec_file(execFind)
+            if  file_prefix == 0:
+                exec_file(execSpl)
                 return
-            else:
+            elif cmd not in commands:
                 error(cmd, cmdSpl)
+            else: error(cmd, cmdSpl)
 
 def main():
+    date = datetime.datetime.now()
+    print(f"{TITLE1}tt-shell{RESET} / {TITLE2}{date}{RESET}")
     while True:
         cmdexec()
 
