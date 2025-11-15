1866ea48a6396b3c1bd3df161eeddfaf82744c66
Tatu Pekkarinen <Tatupek7@gmail.com>
2025-11-15 22:48:30 +0200

    Changes in readability and formatting

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import sys
 import shutil
 import subprocess
 
+commands = {"exit", "echo", "type"}
+
 #executing file
 def exec_file(execFind):
     subprocess.Popen(execFind)
+
 #error message
 def error(cmd, cmdSpl):
     match cmdSpl[0]:
@@ -12,36 +15,40 @@ def error(cmd, cmdSpl):
             print(*cmdSpl[1:],": not found",sep="")
         case _:
             print(f"{cmd}: command not found")
-commands = {"exit", "exit 0", "echo", "type", "./"}
+
+#echo command
 def echo_cmd(cmdSpl):
     print(* cmdSpl[1:])
+
 #exit command
 def exit_cmd(cmdSpl):
     match cmdSpl[0]:
         case "exit":
             sys.exit(0)
         case _:
-            if cmdSpl[0] == "exit" and cmdSpl[1] == "0" :
-                sys.exit(0)
-            else :
-                error(cmdSpl) 
-#type command                
+            error(cmdSpl) 
+
+#type command
 def type_cmd(cmdSpl, cmd):
-    find = shutil.which(cmdSpl[1])
+    if cmd == "type":
+        error(cmdSpl, cmd)
+        return
+    
+    file_find = shutil.which(cmdSpl[1])
     if cmdSpl[1] in commands:
         print(cmdSpl[1], "is a shell builtin")
         return  
-    elif cmdSpl[1] != commands and not find:
+    elif cmdSpl[1] != commands and not file_find:
         error(cmd, cmdSpl)            
-    else: print(cmdSpl[1],"is", find)
+    else: print(cmdSpl[1],"is", file_find)
+
 #executing commands
 def cmdexec():
     sys.stdout.write("$ ")
     cmd = input()
     cmdSpl = cmd.split(" ")
-    execSpl = cmd.split("./")
+    execSpl = cmd.split(".")
     cmdSpl = cmd.split(" ")
-    cmdf = cmd.find("./",0)
 
     match cmdSpl[0]:
         case "echo":
@@ -51,12 +58,13 @@ def cmdexec():
         case "type":
             type_cmd(cmdSpl, cmd)
         case _:
-            if  cmdf == 0:
+            if  cmd == "." + execSpl[1]:
                 execFind = shutil.which(execSpl[1])
                 exec_file(execFind)
                 return
             else:
                 error(cmd, cmdSpl)
+
 def main():
     while True:
         cmdexec()
