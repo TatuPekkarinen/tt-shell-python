0603880148ed84430d6c7a6198e6e3d3dbb2c41f
TP44 <tatupek7@gmail.com>
2026-01-16 21:49:42 +0200

    couple of small fixes

--- main.py
+++ main.py
@@ -18,22 +18,22 @@ script_directory = Path(__file__).parent
 #global deque of command history
 history = deque(maxlen=25)
 
+def scan(current_port, sock_data, status, sock):
+    if status == 0:
+        print(f"Port / {current_port} / {GREEN}{sock_data[str(status)]}{RESET}")
+    elif status > 0: 
+        print(f"Port / {current_port} / {WARNING}{sock_data[str(status)]}{RESET}")
+    else: 
+        print(f"Port / {current_port} / {WARNING}{sock_data[str(status)]}{RESET}")
+    sock.close()
+
 #error handler (work needed)
 def error_handler(command, command_split):
-    print(f"{WARNING}{command}: command not found{RESET}")
+    print(f"{WARNING}Command not found{RESET} => {command}")
     return
 
 #connectivity tester and port scanner
-def connection_scan(command, command_split):
-    def scan():
-        if status == 0:
-            print(f"Port / {current_port} / {GREEN}{sock_data[str(status)]}{RESET}")
-        elif status > 0: 
-            print(f"Port / {current_port} / {WARNING}{sock_data[str(status)]}{RESET}")
-        else: 
-            print(f"Port / {current_port} / {WARNING}{sock_data[str(status)]}{RESET}")
-        sock.close()
-
+def connection_portal(command, command_split):
     def port_valid(port: int) -> bool:
         return 0 <= port <= 65535
 
@@ -48,20 +48,20 @@ def connection_scan(command, command_split):
                 return
 
             print(f"{GREEN}Starting scan from {command_split[2]} to {command_split[3]}{RESET}")
-            for port_range in range(int(command_split[2]), int(command_split[3]) + 1):
+            for port_iterator in range(int(command_split[2]), int(command_split[3]) + 1):
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.settimeout(0.5)
                 LOCALHOST = '127.0.0.1'
-                PORT = int(port_range)
+                PORT = int(port_iterator)
 
                 if not port_valid(PORT):
-                    print(f"Port ({port_range}) invalid / {WARNING}Not in range{RESET}")
+                    print(f"Port ({port_iterator}) invalid / {WARNING}Not in range{RESET}")
                     sock.close()
                     return
                 
                 current_port = PORT
                 status = sock.connect_ex((LOCALHOST, PORT))
-                scan()  
+                scan(current_port, sock_data, status, sock)  
 
         else:   
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -78,7 +78,7 @@ def connection_scan(command, command_split):
 
             current_port = PORT
             status = sock.connect_ex((HOST, PORT))
-            if status >= 0: scan()
+            if status >= 0: scan(current_port, sock_data, status, sock)
             else: error_handler(command, command_split)
     else: error_handler(command, command_split)
 
@@ -103,7 +103,6 @@ def open_website(command, command_split):
         case 2:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(5)
-
             try: HOST = socket.gethostbyname(str(command_split[1]))
 
             except socket.gaierror:
@@ -113,14 +112,12 @@ def open_website(command, command_split):
             HTTP_PORT = 443
             print(f"CONNECTION TEST => {GREEN}Connection to {HOST} from {HTTP_PORT}{RESET}")
             status = sock.connect_ex((HOST, HTTP_PORT))
-
             if status == 0:
                 print(f"CONNECTION SUCCESFUL => {GREEN}Accessing website{RESET} / {command_split[1]}")
                 webbrowser.open(command_split[1])
 
             else: print(f"{WARNING}Connection failed{RESET} / Unable to open website")
             return
-        
         case _: error_handler(command, command_split)
     
 #environment variables   
@@ -128,7 +125,7 @@ def environ_print(command, command_split):
     if len(command_split) == 1:
             envar = os.environ
             pprint.pprint(dict(envar), width=5, indent=5) 
-    else:  error_handler(command, command_split)
+    else: error_handler(command, command_split)
     return
 
 #builtin checker
@@ -155,7 +152,6 @@ def type_command(command, command_split):
 def morph_command(command, command_split):
     morph = list(str(command_split[2]))
     target = list(str(command_split[1]))
-
     morph_value = len(morph) 
     target_value = len(target)
     value = morph_value - target_value
@@ -167,10 +163,9 @@ def morph_command(command, command_split):
         absolute_value = value
 
     if morph is not target:
-        for n in range(absolute_value):
+        for number in range(absolute_value):
             if len(morph) != len(target):
                 for iterator in range(absolute_value):
-
                     if len(target) > len(morph):
                         morph.append(target[iterator])
                         result = "".join(morph)
@@ -188,8 +183,8 @@ def morph_command(command, command_split):
                 return
 
             if len(morph) == len(target):
-                morph[n] = target[n]
-            
+                morph[number] = target[number]
+
     else: 
         print(f"{GREEN}{morph} <=> {target}{RESET}")
         return
@@ -221,15 +216,15 @@ commands = {
     "web": open_website,
     "env": environ_print,
     "file": execute_file,
-    "con": connection_scan,
+    "con": connection_portal,
     "history": modify_history,
     "morph": morph_command
 }
 
 #executing commands
 def command_execute():
-
     sys.stdout.write(f"[{script_directory}]{GREEN} => {RESET}")
+
     try:
         command = input()
         if command == "": return
@@ -248,7 +243,7 @@ def command_execute():
         else: error_handler(command, command_split)
 
     except KeyboardInterrupt: 
-        print(f"\n{WARNING}Keyboard interrupt received{RESET}")
+        print(f"\n{WARNING}Keyboard interrupt => received{RESET}")
         return
 
 def main():
